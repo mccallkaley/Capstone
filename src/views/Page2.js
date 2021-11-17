5db5c0e431a968b35830396fc8e3a81c1ebc3227
Kevin Beier <kevinb@codingtemple.com>
2021-11-16 21:26:13 -0500

    orm and yup end of day2

--- src/views/Page2.js
+++ src/views/Page2.js
@@ -4,7 +4,9 @@ export default class Page2 extends Component {
     render() {
         return (
             <div>
-                Page 2
+                test: {this.props.test}<br/>
+                {/* The user is {this.state.user}<br/> */}
+                <input onChange={(event)=>this.props.setUser(event.target.value)}></input>
             </div>
         )
     }
