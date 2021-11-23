01aa688dff61b1993ea07c9b046440046c9e5668
Kevin Beier <kevinb@codingtemple.com>
2021-11-22 22:10:07 -0500

    crud for cats

--- src/api/apiCategory.js
+++ src/api/apiCategory.js
@@ -9,3 +9,18 @@ export const getCategories = async (token) =>{
     if (response.ok){return response.data.categories}
     return
 }
+
+export const postCategory = async(token, catName)=>{
+    const response = await apiClientWithToken(token).post(endpoint,{name:catName});
+    if (response.ok){return true}else{return false}
+}
+
+export const putCategory = async(token, id, catName)=>{
+    const response = await apiClientWithToken(token).put(endpoint+'/'+id,{name:catName});
+    if (response.ok){return true}else{return false}
+}
+
+export const deleteCategory = async (token, id)=>{
+    const response = await apiClientWithToken(token).delete(endpoint+'/'+id);
+    if (response.ok){return true}else{return false}
+}
